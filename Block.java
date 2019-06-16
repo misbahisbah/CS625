740fa781c5f3d27b7a81c73bd25fff4753ead698
misbahisbah <48205441+misbahisbah@users.noreply.github.com>
2019-06-17 00:16:40 +0500

    Add files via upload

---
+++ Block.java
@@ -0,0 +1,29 @@
+class Block 
+{
+int a,b,c;
+int volume;
+Block(int i, int j, int k)
+{
+a=i;
+b=j;
+c=k;
+volume=a*b*c;
+}
+boolean samevolume(Block ob)
+{
+if(ob.volume==volume) return true;
+else return false;
+}
+}
+class PaasOb 
+{
+public static void main(String args[])
+{
+Block ob1=new Block(10,2,5);
+Block ob2=new Block(10,2,5);
+Block ob3=new Block(4,5,5);
+System.out.println("ob1 same dimensions as ob2: " + ob1.sameBlock(ob2));
+System.out.println("ob1 same dimensions as ob3: " + ob1.sameBlock (ob3));
+System.out.println("ob1 same volume as ob3:" + ob1.samevolume(ob3)); 
+}
+}
