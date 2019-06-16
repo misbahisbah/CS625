6ce625f5d2dcb82e5dfba43572d6c94f7a426f95
misbahisbah <48205441+misbahisbah@users.noreply.github.com>
2019-06-16 23:40:53 +0500

    Add files via upload

---
+++ MyClass.java
@@ -0,0 +1,25 @@
+class MyClass 
+{
+private int alpha;
+public int beta;
+int gamma;
+void setAlpha(int a)
+{
+alpha=a;
+}
+int getalpha() 
+{
+return alpha;
+}
+}
+class AccessDemo
+{
+public static void main(String args[])
+{
+MyClass ob=new MyClass();
+ob.setAlpha(-99);
+System.out.println("ob.alpha is" + ob.getalpha());
+ob.beta=88;
+ob.gamma=99;
+}
+}
